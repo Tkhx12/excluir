31ab7d526cf82431be4fb52e251f77390f3d0ce3
Tkhx <rafaellemosdoamaral749@gmail.com>
2023-04-30 14:17:23 -0300

    Primeiro Commit

---
+++ statement/states.js
@@ -0,0 +1,36 @@
+let x, y, z; // Declara variável com Let sendo elas x, y e z
+x = 5; // Declara X com o valor 5 
+y = 6; // Declara Y com o valor 6
+z = x + y // Declara Z como o resultado das duas somas entre x e y
+console.log(z) // Mostra na log o resultado puxando o Z
+
+// uma semi-Coluna senda essa ; ela serve para separar as instruções usadas acima, quando separada por uma Semi-Coluna você pode por em uma linha que nenhuma vai interferir a outra
+
+// o JavaScript ignora espaços vazios, tornando-o mais legível para quem for ler o código sendo assim exemplos:
+let person = "Hege";
+let person2="Hege";
+// Também serve para operadores aritméticos
+let a = b + c;
+let i = v + o;
+// Para ficar mais legível, os programadores preferem não passar de 80 linhas (na mesma linha)
+// Podendo pular para linha de baixo e fechando com o texto da linha 7 (Semi-Coluna)
+document.getElementById("demo").innerHTML =
+"Hello World!";
+
+// Instruções em JavaScript podem ser escritas juntas desde que estejam dentro de colchetes {}
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello World!";
+    document.getElementById("demo2").innerHTMl = "Hello Space!";
+}
+
+// Abaixo algumas KeyWords que vamos ver mais à frente
+var casa = y . z; // Declara uma variável
+let // Declara uma variável em bloco
+const word = z . y // Declara uma constante em bloco
+if // Marca um bloco de instrução para ser executado em uma condição
+switch // Marca um bloco de instrução para ser executado em diferentes casos
+for // Marca um bloco de instruções para ser executado em loop
+function // Declara uma Função
+return // Sai de uma função/Retorna um valor
+try // Implementa o tratamento de erros para um bloco de instruções
+
